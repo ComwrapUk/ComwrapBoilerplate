7735495942b34058fa92dddbd21ce7e2867d2ee8
ComwrapUkReply <s.sznajder@reply.com>
2024-06-06 16:24:38 +0100

    fix typo splusplus to plusplus

--- blocks/dynamic/dynamic.js
+++ blocks/dynamic/dynamic.js
@@ -6,7 +6,7 @@ import { createOptimizedPicture } from '../../scripts/aem.js';
 import {
   a, div, li, p, h3, span, ul,
 } from '../../plusplus/block-party/dom-helpers.js';
-import ffetch from '../../splusplus/block-party/ffetch.js';
+import ffetch from '../../plusplus/block-party/ffetch.js';
 
 export default async function decorate(block) {
   // Select the element by its class
