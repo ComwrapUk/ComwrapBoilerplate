647002865d6612302e6bd664844ab879aacf5cbb
ComwrapUkReply <s.sznajder@reply.com>
2024-06-11 10:10:49 +0100

    enhance table block

--- blocks/table/table.js
+++ blocks/table/table.js
@@ -1,7 +1,9 @@
+/* eslint-disable no-shadow */
 /*
  * Table Block
  * Recreate a table
  * https://www.hlx.live/developer/block-collection/table
+ * Modified to add aria
  */
 
 function buildCell(rowIndex) {
@@ -14,6 +16,7 @@ export default async function decorate(block) {
   const table = document.createElement('table');
   const thead = document.createElement('thead');
   const tbody = document.createElement('tbody');
+  table.append(thead, tbody);
 
   const header = !block.classList.contains('no-header');
   if (header) {
@@ -24,13 +27,33 @@ export default async function decorate(block) {
   [...block.children].forEach((child, i) => {
     const row = document.createElement('tr');
     if (header && i === 0) thead.append(row);
-    else tbody.append(row);
+    else thead.append(row);
     [...child.children].forEach((col) => {
       const cell = buildCell(header ? i : i + 1);
       cell.innerHTML = col.innerHTML;
       row.append(cell);
     });
   });
+
   block.innerHTML = '';
   block.append(table);
+
+  // Set the role of the table
+  table.setAttribute('role', 'table');
+
+  // Enhance each header cell
+  const headers = table.querySelectorAll('th');
+  headers.forEach((header, index) => {
+    header.setAttribute('scope', 'col');
+    header.setAttribute('role', 'columnheader');
+    header.id = `header-${index}`;
+  });
+
+  // Enhance each data cell
+  const cells = table.querySelectorAll('td');
+  cells.forEach((cell, index) => {
+    const columnIndex = index % table.rows[0].cells.length;
+    const headerId = `header-${columnIndex}`;
+    cell.setAttribute('aria-describedby', headerId);
+  });
 }
