1d01269dce40d2ef3fb12d1f2b8af4d2bd9444a2
ComwrapUkReply <s.sznajder@reply.com>
2024-06-11 10:48:17 +0100

    fix expressions in text block

--- blocks/text/text.js
+++ blocks/text/text.js
@@ -1,5 +1,6 @@
+/* eslint-disable import/no-relative-packages */
 /* eslint-disable no-unused-vars */
-import { renderExpressions } from '../../../../../../../plusplus/plugins/expressions/src/expressions.js';
+import { renderExpressions } from '../../plusplus/plugins/expressions/src/expressions.js';
 
 export default function decorate(block) {
   renderExpressions(document.querySelector('.text-wrapper'));
