3a3fa8538b8754ebb46e800889bf731aa403bde6
ComwrapUkReply <s.sznajder@reply.com>
2024-06-13 11:51:38 +0100

    refresh plusplus

--- plusplus/src/clientExpressions.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable import/no-relative-packages */
-import { createExpression } from '../plugins/expressions/src/expressions.js';
-
-// a sample expression, expands the text in the siteConfig, from the args
-createExpression('expand', ({ args }) => window.siteConfig?.[args]);
