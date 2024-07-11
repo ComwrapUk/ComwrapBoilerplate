465b760d8f62b78d00fc966012edb72c577ea032
ComwrapUkReply <s.sznajder@reply.com>
2024-07-11 14:16:29 +0100

    update download script

--- blocks/download/download.js
+++ blocks/download/download.js
@@ -1,6 +1,4 @@
 export default function decorate(block) {
   const downloadLink = block.querySelector('.button-container a');
-  if (downloadLink) {
-    downloadLink.setAttribute('download', '');
-  }
+  downloadLink.setAttribute('download', '');
 }
