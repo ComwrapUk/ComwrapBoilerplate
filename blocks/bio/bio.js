e7db8e5bc65545e29ccb41018510358a4c5a9b56
ComwrapUkReply <s.sznajder@reply.com>
2024-07-11 14:50:37 +0100

    update expressions path

--- blocks/bio/bio.js
+++ blocks/bio/bio.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-relative-packages */
 /* eslint-disable no-unused-vars */
-import { renderExpressions } from '../../plusplus/plugins/expressions/src/expressions.js';
+import { renderExpressions } from '../../../../../../../../../plusplus/plugins/expressions/src/expressions.js';
 
 export default function decorate(block) {
   const bioElement = document.querySelector('.bio');
